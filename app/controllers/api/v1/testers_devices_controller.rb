df26d335f71ff397a8011c2793808c0d66f35662
Mark Kevin King <king.mark.kevin@icloud.com>
2017-09-18 18:47:36 +0800

    Fix Syntax Error

--- app/controllers/api/v1/testers_devices_controller.rb
+++ app/controllers/api/v1/testers_devices_controller.rb
@@ -1,6 +1,6 @@
 module Api
   module V1
-    module TestsDevicesController < ApplicationController
+    class TestsDevicesController < ApplicationController
       before_action :authenticate_request
             
       def add_tester_device
